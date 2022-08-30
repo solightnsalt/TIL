b801bd58c49b54cb2e2d59d7f35be6b09894e303
solightnsalt <soldakm@gmail.com>
2022-08-30 18:14:50 +0900

    0830

--- Python/Codeup_100/codeup_6098.py
+++ Python/Codeup_100/codeup_6098.py
@@ -3,21 +3,7 @@ sys.stdin = open('6098.txt')
 from pprint import pprint
 
 metrix = [list(map(int, input().split())) for _ in range(10)]
-# pprint(metrix)
 x, y = 1, 1
-# metrix[x][y] = 9 #시작
-# for i in range(9): 
-#     for j in range(9):
-#         if metrix[x][y+j] == 0:
-#             y += j
-#             metrix[x][y] = 9
-#         elif metrix[x][y+j] == 1:
-#             x += i
-#             metrix[x][y] =9
-#         else:
-#             break
-                                
-# pprint(metrix)
 
 while True:
     if (metrix[x][y] == 0):
@@ -34,6 +20,6 @@ while True:
     elif (metrix[x+1][y] != 1):
         x = x + 1
 
-for i in range(9):
+for i in range(10):
     print(*metrix[i])
 
