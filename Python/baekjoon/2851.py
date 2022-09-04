2c089628398c981b04380dc5f85ebd0f05bee8dc
solightnsalt <soldakm@gmail.com>
2022-09-04 23:25:14 +0900

    0904

--- Python/baekjoon/2851.py
+++ Python/baekjoon/2851.py
@@ -1,2 +1,12 @@
 import sys
 sys.stdin = open('2851.txt')
+
+mushroom = [int(input()) for _ in range(10)]
+score = 0
+for i in mushroom:
+    score += i
+    if score >= 100:
+        if score - 100 > 100 - (score - i):
+            score -= i
+            break
+print(score)
