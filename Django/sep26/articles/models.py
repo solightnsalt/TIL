31dd54ace226384c61a5e80c9ed88cc9e07138c1
solightnsalt <soldakm@gmail.com>
2022-09-28 16:19:22 +0900

    0928

--- Django/sep26/articles/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Article(models.Model):
-    content = models.TextField()
-
-
-# python manage.py makemigrations
-# python manage.py migrate
